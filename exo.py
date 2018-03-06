b06546c9333be03fc37eaef74f62bbfcc672a1af
bernard169 <bernard@tourneur.org>
2018-03-06 13:04:34 +0100

    ex4 multithreading complete

--- exo.py
+++ exo.py
@@ -1,4 +1,6 @@
 import socket
+import subprocess
+
 
 vinci = socket.socket ()
 vinci.connect (('www.vinci.be',80))  #80 est le port
@@ -11,3 +13,23 @@ response = vinci.recv(512).decode()
 print (response)
 vinci.close()
 
+import random
+import threading as th
+#Exercice 4
+
+lock = th.Lock ()
+L= []
+
+def throwDice (myList,c):
+    lock.acquire ()
+    myList.append (random.randint (1,6))
+    lock.release ()
+
+threads = [th.Thread (target=throwDice, args=(L,counter)) for counter in range (1,1000)]
+for thread in threads:
+    thread.start ()
+for thread in threads:
+    thread.join ()
+
+avg = (sum (L)) / len (L)
+print (avg)
